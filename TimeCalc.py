b42956e5d47ad5558a3d8e19d253bbb30fe90e98
Theorin F <43284735+TFGrump@users.noreply.github.com>
2021-03-02 02:58:06 -0500

    Update TimeCalc.py

--- TimeCalc.py
+++ TimeCalc.py
@@ -24,7 +24,7 @@ def parse_tuple(time):
 
 # Example use
 hw_times = [(0, 8, 8), (0, 9, 12), (0, 6, 9), (0, 5, 5), (0, 5, 3), (0, 3, 30), (0, 8, 19), (0, 7, 54)]
-print("total hw: " + parse_tuple(add_time(hw_times)))
+print("Total hw: " + parse_tuple(add_time(hw_times)))
 
 sleep_times = [(0, 3, 51), (0, 5, 14), (0, 13, 33), (0, 8, 30), (0, 2, 29), (0, 7, 24),
                (0, 11, 6), (0, 8, 24), (0, 9, 34), (0, 10, 19), (0, 9, 12)]
@@ -55,3 +55,27 @@ print("Total hang out: " + parse_tuple(add_time(friend_times)))
 
 misc_times = [(0, 2, 0), (0, 1, 6), (0, 0, 8), (0, 5, 30)]
 print("Total Misc: " + parse_tuple(add_time(misc_times)))
+
+convert_time = [(0, 88, 0)]
+print("88 hours is: " + parse_tuple(add_time(convert_time)))
+
+total_time = [(2, 5, 20), (3, 17, 36), (0, 18, 39), (0, 5, 53), (0, 10, 41),
+              (1, 2, 21), (2, 7, 1), (0, 11, 35), (0, 15, 20), (0, 8, 44)]
+print("Total Time: " + parse_tuple(add_time(total_time)))
+
+"""
+Output of the above print statements:
+
+Total hw: Days: 2, Hours: 5, Minutes: 20
+Total sleep: Days: 3, Hours: 17, Minutes: 36
+Total warframe: Days: 0, Hours: 18, Minutes: 39
+Total osu!: Days: 0, Hours: 5, Minutes: 53
+Total DbD: Days: 0, Hours: 10, Minutes: 41
+total other games: Days: 1, Hours: 2, Minutes: 21
+Total food: Days: 2, Hours: 7, Minutes: 1
+Total lecture: Days: 0, Hours: 11, Minutes: 35
+Total hang out: Days: 0, Hours: 15, Minutes: 20
+Total Misc: Days: 0, Hours: 8, Minutes: 44
+88 hours is: Days: 3, Hours: 16, Minutes: 0
+Total Time: Days: 2, Hours: 16, Minutes: 55
+"""
